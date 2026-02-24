50efaf3904ca039285b4d935e12bc95f6d4bf38f
unknown <nkm199958@gmail.com>
2026-02-24 20:52:29 +0900

    수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -8,8 +8,9 @@ with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
-    catchup=False, # 누락된 구간을 돌릴건지(True 시 누락 부분 한꺼번에 돌림)
+    catchup=False # 누락된 구간을 돌릴건지(True 시 누락 부분 한꺼번에 돌림)
 ) as dag:
+    
     def select_fruit():
         fruit = ['apple','banana','orange','avocado']
         rand_int = random.randint(0,3)
@@ -17,7 +18,7 @@ with DAG(
 
     
     py_t1 = PythonOperator(
-        task_id = 'py_t1'm
+        task_id = 'py_t1'm,
         python_callable = select_fruit
     )
 
